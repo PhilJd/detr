adc49a59319e52b6d6637fb9af52106f06ec1eb0
Philipp Jund <ijund.phil@gmail.com>
2020-09-07 09:52:29 +0200

    Halve input dimensions.

--- detr/datasets/coco.py
+++ detr/datasets/coco.py
@@ -119,17 +119,18 @@ def make_coco_transforms(image_set):
         T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
-    scales = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
+    original_scales = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
+    scales = [s * 0.5 for s in original_scales]
 
     if image_set == 'train':
         return T.Compose([
             T.RandomHorizontalFlip(),
             T.RandomSelect(
-                T.RandomResize(scales, max_size=1333),
+                T.RandomResize(scales, max_size=640),  # Originally: 1333.
                 T.Compose([
-                    T.RandomResize([400, 500, 600]),
-                    T.RandomSizeCrop(384, 600),
-                    T.RandomResize(scales, max_size=1333),
+                    T.RandomResize([200, 250, 650]),   # Originally: [400, 500, 600].
+                    T.RandomSizeCrop(192, 300),  # Originally: 384, 600.
+                    T.RandomResize(scales, max_size=640),   # Originally: 1333.
                 ])
             ),
             normalize,
@@ -137,7 +138,7 @@ def make_coco_transforms(image_set):
 
     if image_set == 'val':
         return T.Compose([
-            T.RandomResize([800], max_size=1333),
+            T.RandomResize([480], max_size=640),  # Originally: [800], 1333.
             normalize,
         ])
 
